738dae829e87046beb0e18a7c49cc7f05c3db99d
tajciglar <taj.brzovic@gmail.com>
2024-10-17 11:04:12 +0200

    Log in page style fix

--- frontend-users/src/components/LogInPage.jsx
+++ frontend-users/src/components/LogInPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
-import './logInPage.css';
+import '../styles/logInPage.css';
 
 const LogInPage = () => {
     const navigate = useNavigate();
